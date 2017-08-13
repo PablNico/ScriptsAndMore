011156d28569b922b46b2c85ec0fe15b1e78a7a9
PablNico <nico_camboriu@hotmail.com>
2017-08-13 20:26:22 -0300

    as

--- PHP-exemplos/area_circulo.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$pi = 3.14159;
-$raio = 5.2;
-$area = $pi*pow($raio,2);
-echo 'Área = '.$area;
-echo '<br>Área formatada = ' .number_format($area,2);
-
-?>
--- PHP-exemplos/variaveis_exemplo1.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$caixa = 10;
-	echo "O caixa vale ".$caixa."<br>";
-	$caixa = "dez";
-	echo "O caixa vale ".$caixa."<br>";
-?>
