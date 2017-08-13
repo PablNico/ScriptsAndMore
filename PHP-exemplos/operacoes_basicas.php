f89a13ed431d985d08b4b0aa87460eaf6ab21cb1
PablNico <nico_camboriu@hotmail.com>
2017-08-13 18:42:09 -0300

    Update operacoes_basicas.php

--- PHP-exemplos/operacoes_basicas.php
+++ PHP-exemplos/operacoes_basicas.php
@@ -4,19 +4,19 @@ $b = 29;
 
 //soma
 $resultado = $a+$b;
-echo "O Resultada da soma entre ".$a.' e '.$b. " é: ".$resultado;
+echo "O Resultado da soma entre ".$a.' e '.$b. " é: ".$resultado;
 
 
 //subtração
 $resultado = $a-$b;
-echo "<br>O Resultada da subtração entre ".$a.' e '.$b. " é: ".$resultado;
+echo "<br>O Resultado da subtração entre ".$a.' e '.$b. " é: ".$resultado;
 
 //multiplicação
 $resultado = $a*$b;
-echo "<br>O Resultada da multiplicação entre ".$a.' e '.$b. " é: ".$resultado;
+echo "<br>O Resultado da multiplicação entre ".$a.' e '.$b. " é: ".$resultado;
 
 // divisão
 $resultado = $a/$b;
-echo "<br>O Resultada da divisão entre ".$a.' e '.$b. " é: ".number_format($resultado,2);
+echo "<br>O Resultado da divisão entre ".$a.' e '.$b. " é: ".number_format($resultado,2);
 
 ?>
